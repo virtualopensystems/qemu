0bc294a604a23b9d07590eb585cafb2b67bea76d
Christoffer Dall <c.dall@virtualopensystems.com>
2013-01-25 19:57:19 -0500

    arm: kvm: Update header
    Signed-off-by: Christoffer Dall <c.dall@virtualopensystems.com>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -636,7 +636,7 @@ struct kvm_ppc_smmu_info {
 #define KVM_CAP_IRQFD_RESAMPLE 82
 #define KVM_CAP_PPC_BOOKE_WATCHDOG 83
 #define KVM_CAP_PPC_HTAB_FD 84
-#define KVM_CAP_SET_DEVICE_ADDR 85
+#define KVM_CAP_SET_DEVICE_ADDR 88
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
