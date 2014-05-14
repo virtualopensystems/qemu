72fa0d5c478aed137d6b24698e457b5361e675fa
Hu Tao <hutao@cn.fujitsu.com>
2014-05-14 17:43:35 +0800

    hmp: add info memdev
    This is the hmp counterpart of qmp query-memdev.
    
    Signed-off-by: Hu Tao <hutao@cn.fujitsu.com>

--- hmp.h
+++ hmp.h
@@ -97,5 +97,6 @@ void object_add_completion(ReadLineState *rs, int nb_args, const char *str);
 void object_del_completion(ReadLineState *rs, int nb_args, const char *str);
 void device_add_completion(ReadLineState *rs, int nb_args, const char *str);
 void device_del_completion(ReadLineState *rs, int nb_args, const char *str);
+void hmp_info_memdev(Monitor *mon, const QDict *qdict);
 
 #endif
